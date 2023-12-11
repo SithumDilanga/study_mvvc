16ffceb7f4e2a7c0de265fabd486842085a455e5
Sithum Dilanga <49783317+SithumDilanga@users.noreply.github.com>
2023-12-11 10:19:49 +0900

    Initial Commit

---
+++ study_mvvc/study_mvvcApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_mvvcApp.swift
+//  study_mvvc
+//
+//  Created by Mirissa Wellalage　Tashmika Isuri on 2023/12/11.
+//
+
+import SwiftUI
+
+@main
+struct study_mvvcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
